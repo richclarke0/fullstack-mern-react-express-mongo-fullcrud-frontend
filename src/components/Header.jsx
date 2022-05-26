332288f27030adf3f0e85b63feff609b98e5df02
Richard Clarke <freshnewrich@gmail.com>
2022-05-25 19:52:42 -0700

    styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"
+
 function Header(props) {
-    return <h1>headre</h1>
+    return (
+      <nav className="nav">
+        <Link to={"/"}>
+          <div>People App</div>
+        </Link>
+      </nav>
+    )
   }
   
   export default Header
